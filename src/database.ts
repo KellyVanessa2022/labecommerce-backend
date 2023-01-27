86aa3700e6131e59a968405e7555f935d9abe8b6
KellyVanessa2022 <kellyvanessa2017@gmail.com>
2023-01-27 16:11:09 -0300

    feito

--- src/database.ts
+++ src/database.ts
@@ -1,83 +1,83 @@
-
-import { TUser, TProduct, TPurchase, CATEGORY_PURCHASE} from "./types";
+import { TUser, TProduct, TPurchase , PRODUCT_CATEGORY } from "./types"
 
 export const users : TUser[] = [
     {
         id: "1",
-        email:"cliente1@gmail.com",
-        password: "dsope5687e", 
+        email: "vinnysantos@mail.com",
+        password: "f56f"
     },
     {
         id: "2",
-        email:"cliente2@gmail.com",
-        password: "oepqw5d6e", 
-    }
-]
-export const products : TProduct[] =[
-    {
-        id: "1",
-        name:"tablet",
-        price: 2000,
-        category: CATEGORY_PURCHASE.ACCESSORIES,
-    },
-    {
-        id: "2",
-        name:"bolsas",
-        price: 450,
-        category: CATEGORY_PURCHASE.CLOTHES_AND_SHOES,
+        email: "pedro1@mail.com",
+        password: "d8e9r"
     },
     {
         id: "3",
-        name:"teclado",
-        price: 99,
-        category: CATEGORY_PURCHASE.ELECTRONICS,
-    },
-]
-export const purchases : TPurchase[] =[
+        email:"Rick564@gmail.com",
+        password: "sdw8d"
+    }
+];
+
+export const products : TProduct[] = [
     {
-        userId:"1",
-    productId:"tablet",
-    quantity:50,
-    totalPrice:180,
-    },
-    {
-        userId:"2",
-    productId:"bolsas",
-    quantity:45,
-    totalPrice:120,
+        id: "1",
+        name: "Teclado",
+        price: 9,
+        category: PRODUCT_CATEGORY.ACCESSORIES
     },
     {
-        userId:"3",
-    productId:"teclado",
-    quantity:32,
-    totalPrice:110,
+        id: "2",
+        name: "Fone",
+        price: 6,
+        category: PRODUCT_CATEGORY.ACCESSORIES
     },
+    {
+        id: "3",
+        name: "Mouse",
+        price: 7,
+        category: PRODUCT_CATEGORY.ACCESSORIES
+    }
 ]
 
-export function createUser(id:string, email:string, password: string): string{
+export const purchases : TPurchase[] = [
+    {
+        userId: "1",
+        productId: "1",
+        quantity: 3,
+        totalPrice: 9
+    },
+    {
+        userId: "2",
+        productId: "2",
+        quantity: 5,
+        totalPrice: 45
+    }
+]
+
+export function createUser(id : string, email : string, password : string) : string{
     users.push({
         id,
         email,
         password
-    })
-    return("Cadastro realizado com Sucesso")
+    });
+    return ("Cadastro realizado com sucesso");
 }
 
-export function getAllUsers(): TUser[]{
-    return users
+export function getAllUsers() : TUser[]{
+    return users;
 }
 
-export function createProduct(id: string, name: string, price: number, category: CATEGORY_PURCHASE): string{
+export function createProduct(id : string, name : string, price : number, category : PRODUCT_CATEGORY) : string{
     products.push({
         id,
         name,
         price,
         category
     })
-    return ("Produto criado com sucesso")
+    return ("Produto criado com sucesso");
 }
 
-export function acessarProducts(): TProduct[]{
+export function acessarProducts() : TProduct[]{
     return products;
 }
 
@@ -88,18 +88,17 @@ export function acessarProductId(id : string) : (undefined | TProduct){
 export function acessarProductsName(q : string) : TProduct[]{
     return products.filter(product => product.name.toLowerCase().includes(q.toLowerCase()));
 }
-    
-export function createPurchase(userId : string, productId : string, quantity : number, totalPrice : number): string{
-    
+
+export function createPurchase(userId : string, productId : string, quantity : number, totalPrice : number) : string{
     purchases.push({
-    userId,
-    productId,
-    quantity,
-    totalPrice
+        userId,
+        productId,
+        quantity,
+        totalPrice    
     })
     return ("Compra realizada com sucesso");
 }
 
-export function getAllPurchasesFromUserId(userIdToSearch: string): TPurchase[]{
+export function getAllPurchasesFromUserId(userIdToSearch : string) : TPurchase[]{
     return purchases.filter(purchase => purchase.userId === userIdToSearch);
 }
--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,161 +1,202 @@
 -- Active: 1674579802581@@127.0.0.1@3306
-CREATE TABLE users (
-	id TEXT PRIMARY KEY UNIQUE NOT NULL,
-	email TEXT UNIQUE NOT NULL,
-	password TEXT NOT NULL
-);
 
---Populando tabela de users
-INSERT INTO users(id, email, password)
-VALUES("a001", "kelly@gmail.com", "221l8o7");
+CREATE TABLE
+    users (
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    );
 
-INSERT INTO users(id, email, password)
-VALUES("a002", "maria@gmail.com", "031khe8");
-
-INSERT INTO users(id, email, password)
-VALUES("a003", "vinnysantos@gmail.com", "seqw78e9qd");
-
-
-
---excluir tabela
 DROP TABLE users;
 
+INSERT INTO
+    users(id, email, password)
+VALUES
+(
+        "a001",
+        "kellyvanessa@gmail.com",
+        "s2q8e952f10"
+    );
 
+INSERT INTO
+    users(id, email, password)
+VALUES
+(
+        "a002",
+        "00souza@gmail.com",
+        "4a8wr51f"
+    );
 
-CREATE TABLE products (
-id TEXT PRIMARY KEY UNIQUE NOT NULL,
-	name TEXT  NOT NULL,
-	price REAL NOT NULL,
-    category TEXT NOT NULL
- );
+INSERT INTO
+    users(id, email, password)
+VALUES
+(
+        "a003",
+        "santosvinny@gmail.com",
+        "2z32deew"
+    );
 
-INSERT INTO products(id, name, price, category)
-VALUES("f001",  "mouse", 50, "eletronicos");
+DROP TABLE products;
 
-INSERT INTO products(id, name, price, category)
-VALUES("f002","teclado", 120, "eletronicos");
+CREATE TABLE
+    products (
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        name TEXT NOT NULL,
+        price REAL NOT NULL,
+        category TEXT NOT NULL
+    );
 
-INSERT INTO products(id, name, price, category)
-VALUES("f003","fone de ouvido", 100, "eletronicos");
+INSERT INTO
+    products(id, name, price, category)
+VALUES
+("a01", "mouse", 130, "eletronicos");
 
-INSERT INTO products(id, name, price, category)
-VALUES("f004","webcam", 290, "eletronicos");
+INSERT INTO
+    products(id, name, price, category)
+VALUES
+("a02", "teclado", 200, "eletronicos");
 
-INSERT INTO products(id, name, price, category)
-VALUES("f005","mousepad", 30, "eletronicos");
+INSERT INTO
+    products(id, name, price, category)
+VALUES
+("a03", "fone de ouvido", 299, "eletronicos");
 
-SELECT *  from products;
+INSERT INTO
+    products(id, name, price, category)
+VALUES
+("a04", "webcam", 599, "eletronicos");
 
-UPDATE products 
-SET name = "suporte de notebook", price =200
-WHERE id = "f002";
+INSERT INTO
+    products(id, name, price, category)
+VALUES
+("a05", "suporte para notebook", 90, "eletronicos");
 
-UPDATE products
-SET price = 200
-WHERE id = "f002";
+SELECT * from products;
 
+UPDATE products SET name = "couro", price =100 WHERE id = "a02";
 
-SELECT *  from products;
-SELECT *  from users;
+UPDATE products SET price = 299 WHERE id = "a02";
 
-SELECT * FROM products
-WHERE name ="mouse";
+SELECT * from products;
 
-INSERT INTO users(id, email, password)
-VALUES("a006", "aline@labenu.com", "4685iyr8u9");
+SELECT * from users;
 
+SELECT * FROM products WHERE name ="mousepad";
 
-INSERT INTO products(id, name, price, category)
-VALUES("f006", "notebook", 3220, "eletronicos");
+INSERT INTO
+    users(id, email, password)
+VALUES
+(
+        "a06",
+        "luizfelipe@gmail.com",
+        "23\4z8x5"
+    );
 
-SELECT *  from products;
+INSERT INTO
+    products(id, name, price, category)
+VALUES
+(
+        "a006",
+        "tenis",
+        150.99,
+        "calcados"
+    );
 
-SELECT * FROM products
-WHERE id= "f006";
+SELECT * from products;
 
-DELETE  FROM users
-WHERE id="a006";
+SELECT * FROM products WHERE id= "a006";
 
+DELETE FROM users WHERE id="a06";
 
-DELETE FROM products
-WHERE id = "a006";
+DELETE FROM products WHERE id = "a006";
 
-UPDATE users SET email="jorge@gmail.com", password="1325j5h8" WHERE id="a001";
+UPDATE users
+SET
+    email = "fabiosouza2023@gmail.com",
+    password = "z22a5eq"
+WHERE id = "a001";
 
+UPDATE products SET price=1055.99 WHERE id="a02";
 
-UPDATE products SET price=200 WHERE id="f002";
+SELECT * FROM users ORDER BY email asc;
 
-SELECT * FROM users
-ORDER BY email asc;
+SELECT * FROM products ORDER BY price asc LIMIT 20;
 
-
-SELECT * FROM products
-ORDER BY price asc LIMIT 20;
-
-SELECT * FROM products
-WHERE price >= "2" AND price <="150"
+SELECT *
+FROM products
+WHERE
+    price >= "250"
+    AND price <= "10100"
 ORDER BY price ASC;
 
-
-CREATE TABLE purchases(
-	id TEXT PRIMARY KEY UNIQUE NOT NULL,
-	total_price REAL UNIQUE NOT NULL, 
-	paid INTEGER NOT NULL,
-	delivered_at TEXT,
-	buyer_id TEXT NOT NULL, 
-	 FOREIGN KEY (buyer_id) REFERENCES users (id)
- 
-);
-
-INSERT INTO purchases(id, total_price, paid, delivered_at, buyer_id)
-VALUES("p001",10, 0,"","f001"),
-("p002",20, 0,"","f002"),
-("p003",30, 0,"","f001"),
-("p004",40, 0,"","f003");
-
-SELECT * FROM purchases;
 DROP TABLE purchases;
 
-UPDATE purchases
-SET delivered_at=DATETIME("now", "localtime")
-WHERE id ="p001";
+CREATE TABLE
+    purchases(
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        total_price REAL UNIQUE NOT NULL,
+        paid INTEGER NOT NULL,
+        delivered_at TEXT,
+        buyer_id TEXT NOT NULL,
+        FOREIGN KEY (buyer_id) REFERENCES users (id)
+    );
 
-SELECT * FROM purchases
-INNER JOIN users
-ON purchases.buyer_id =users.id
-WHERE users.id="a001";
-
-CREATE TABLE purchases_products (
-	purchase_id TEXT NOT NULL, 
-	product_id TEXT NOT NULL, 
-	quantity INTEGER NOT NULL,
-	FOREIGN KEY (purchase_id) REFERENCES purchases(id)
-	FOREIGN KEY (product_id) REFERENCES products(id)
-
-
-);
-INSERT INTO purchases_products(purchase_id, product_id, quantity)
+INSERT INTO
+    purchases(
+        id,
+        total_price,
+        paid,
+        delivered_at,
+        buyer_id
+    )
 VALUES
-("p001", "f003", 1),
-("p002", "f003", 2),
-("p003", "f003", 3);
+("001", 100, 0, "", "a001"), ("002", 200, 0, "", "a002"), ("003", 300, 0, "", "a001"), ("004", 400, 0, "", "a003");
+
+SELECT * FROM purchases;
+
 DROP TABLE purchases_products;
 
-SELECT * FROM purchases
-INNER JOIN  purchases_products  
-ON  purchases_products.purchase_id =purchases.id
-;
-SELECT 
-purchases.id AS purchaseId,
-purchases.total_price, 
-purchases.paid,
-purchases.delivered_at, 
-purchases.buyer_id AS buyerId,
-products.id, 
-products.name,
-products.price
+UPDATE purchases
+SET
+    delivered_at = DATETIME("now", "localtime")
+WHERE id = "002";
+
+SELECT *
 FROM purchases
-LEFT JOIN purchases_products
-ON purchases_products.purchase_id = purchases.id
-INNER JOIN products
-ON  purchases_products.product_id = products.id;
+    INNER JOIN users ON purchases.buyer_id = users.id
+WHERE users.id = "a001";
+
+CREATE TABLE
+    purchases_products (
+        purchase_id TEXT NOT NULL,
+        product_id TEXT NOT NULL,
+        quantity INTEGER NOT NULL,
+        FOREIGN KEY (purchase_id) REFERENCES purchases(id) FOREIGN KEY (product_id) REFERENCES products(id)
+    );
+
+INSERT INTO
+    purchases_products(
+        purchase_id,
+        product_id,
+        quantity
+    )
+VALUES ("001", "a03", 1), ("002", "a03", 2), ("003", "a03", 3);
+
+DROP TABLE purchases_products;
+
+SELECT *
+FROM purchases
+    INNER JOIN purchases_products ON purchases_products.purchase_id = purchases.id;
+
+SELECT
+    purchases.id AS purchaseId,
+    purchases.total_price,
+    purchases.paid,
+    purchases.delivered_at,
+    purchases.buyer_id AS buyerId,
+    products.id,
+    products.name,
+    products.price
+FROM purchases
+    LEFT JOIN purchases_products ON purchases_products.purchase_id = purchases.id
+    INNER JOIN products ON purchases_products.product_id = products.id;
